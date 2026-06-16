c588b24c5fc74246b87b3e3cddc561dc92e3614a
BasuraGit <Jan.Mahnke@t-online.de>
2026-06-16 18:14:22 +0200

    Dokumentation generiert und einige kleine improvements

--- game/Spiel.py
+++ game/Spiel.py
@@ -1,31 +1,124 @@
 from game.Spielfeld import Spielfeld
-from game.Schwierigkeit import Schwierigkeit
 from game.Timer import Timer
 from ui.SpielEingabe import SpielEingabe
 from ui.SpielAusgabe import SpielAusgabe
+from ui.ZwischenMenue import ZwischenMenue
+from util.Enums import ErfolgsEnum, ProgrammZustand
+
 
 class Spiel:
-    def __init__(self, konfiguration):
+    """
+    Steuert den Ablauf einer Spielsitzung.
+
+    Die Klasse koordiniert die Zusammenarbeit der einzelnen Komponenten,
+    wie Spielfeld, Ein- und Ausgabe sowie die Menüsteuerung.
+    """
+
+    def __init__(self, konfiguration, haupt_menue):
+        """
+        Initialisiert eine neue Spielinstanz.
+
+        Parameter:
+            konfiguration: Enthält die aktuellen Spieleinstellungen.
+            haupt_menue: Referenz auf das Hauptmenü, um nach Spielende
+                          dorthin zurückkehren zu können.
+        """
         self.konfiguration = konfiguration
+
+        # Erzeugt das Spielfeld auf Basis der aktuellen Konfiguration.
         self.spielfeld = Spielfeld(self.konfiguration)
+
+        # Speichert eine Referenz auf den aktuellen Schwierigkeitsgrad.
         self.schwierigkeit = self.konfiguration.schwierigkeit
+
+        # Zählt die aktuell gespielte Runde.
         self.aktuelle_runde = 1
+
+        # Zuständig für die Konsolenausgabe.
         self.spiel_ausgabe = SpielAusgabe()
+
+        # Zuständig für die Eingaben des Nutzers.
         self.spiel_eingabe = SpielEingabe()
+
+        # Menü zwischen zwei Spielrunden.
+        self.zwischen_menue = ZwischenMenue()
+
+        # Referenz auf das Hauptmenü.
+        self.haupt_menue = haupt_menue
+
+        # Timer für die Zeitbegrenzung.
         # self.timer = Timer()
-    
+
     def starten(self):
+        """
+        Startet eine neue Spielrunde.
+
+        Das Spielfeld wird erzeugt und anschließend in der Konsole
+        dargestellt.
+
+        Ein ablaufender Timer und die Nutzereingabe werden als Threads erzeugt.
+        """
         self.spielfeld.generieren()
         self.spiel_ausgabe.zeige_spiel(self.spielfeld)
-        # self.timer.start(self.konfiguration.timer_max)
-        self.spiel_eingabe.start()
-    
+
+        # TODO: TIMER und Eingabe als Threads umsetzen,
+        # um beides gleichzeitig zu machen.
+
+        # completion_event ? threading.Event()
+        #
+        # next_state=list()
+        # timer_thread = Thread(target=self.timer.start, args=(self.konfiguration.timer_max, completion_event, next_state))
+        # input_thread = Thread(target=self.spiel_eingabe.start, args=(completion_event, next_state))
+        # timer_thread.start()
+        # input_thread.start()
+        #
+        # End based on threading.Event() with completion_event.wait()
+        # in thread: completion_event.set()
+        #
+        # more research regarding cleanup and its interaction with event based ending necessary
+        #
+        # return next_state[0]
+
     def neue_runde(self):
-        pass
-    
-    def auswerten(self):
-        pass
+        """
+        Startet eine weitere Spielrunde.
+
+        Vor Beginn der neuen Runde wird die maximale Suchzeit
+        reduziert, um den Schwierigkeitsgrad schrittweise zu erhöhen.
+        Die Rundenanzahl wird mitgezählt.
+        """
+        self.aktuelle_runde += 1
+        self.konfiguration.reduce_timer_max()
+        self.starten()
+
+    def auswerten(self, erfolgscode: ErfolgsEnum):
+        """
+        Wertet das Ergebnis der Spielrunde aus.
+
+        Zunächst wird eine Rückmeldung ausgegeben.
+        Anschließend wird abhängig vom Ergebnis das passende Menü
+        aufgerufen.
+
+        Parameter:
+            erfolgscode: Enum-Wert, der das Ergebnis der Runde beschreibt.
+        """
+        self.spiel_ausgabe.zeige_rueckmeldung(erfolgscode)
+
+        if (erfolgscode == ErfolgsEnum.RIGHTINPUT):
+            return ProgrammZustand.ZWISCHENMENUE
+        else:
+            return ProgrammZustand.HAUPTMENUE
 
     def beenden(self):
-        pass
+        """
+        Beendet die aktuelle Spielrunde.
 
+        Der Ablauf entspricht einem Zeitüberschreitungsereignis.
+        Die Auswertung erfolgt daher mit dem entsprechenden
+        Erfolgsstatus.
+
+        Die Beendigung des Timer-Threads ist als zukünftige
+        Erweiterung vorgesehen.
+        """
+        # Timer thread beenden
+        self.auswerten(ErfolgsEnum.TIMEOUT)
--- game/Spielfeld.py
+++ game/Spielfeld.py
@@ -1,26 +1,92 @@
 from util import Zufallsgenerator as zg
 
+
 class Spielfeld:
+    """
+    Repräsentiert das Spielfeld einer Spielrunde.
+
+    Die Klasse ist für die Generierung des Zeichenfeldes zuständig.
+    Sie bestimmt das Zielsymbol, dessen Position und erzeugt die
+    zufälligen Inhalte des Spielfelds.
+    """
+
     def __init__(self, konfiguration):
+        """
+        Initialisiert das Spielfeld mit der übergebenen Konfiguration.
+
+        Parameter:
+            konfiguration: Enthält die Einstellungen des Spiels,
+                            insbesondere den aktuellen Schwierigkeitsgrad.
+        """
         self.konfiguration = konfiguration
         self.sk = self.konfiguration.schwierigkeit
         self.feld = list()
 
     def generieren(self):
-        zielsymbolarray = zg.generiere_random_chars(self.sk.zielsymbollaenge)
+        """
+        Erzeugt ein neues Spielfeld.
+
+        Der Ablauf besteht aus folgenden Schritten:
+        - Generierung eines zufälligen Zielsymbols.
+        - Bestimmung einer zufälligen Zielposition.
+        - Erzeugung aller Spielfeldzeilen.
+        - Platzierung des Zielsymbols an der Zielposition.
+        - Auffüllen der übrigen Felder mit Zufallszeichen.
+        """
+        # Leert das Feld für eine neue Generierung
+        self.feld.clear()
+
+        # Erzeugt die Zeichen, aus denen das Zielsymbol besteht.
+        zielsymbolarray = zg.generiere_random_chars(
+            self.sk.zielsymbollaenge
+        )
+
+        # Setzt die einzelnen Zeichen zu einem String zusammen.
         self.zielsymbol = str()
+
         for char in zielsymbolarray:
             self.zielsymbol += char
+
+        # Ermittelt die Position des Zielsymbols innerhalb des Spielfelds.
         self.zielposition = zg.generiere_Position(self.sk)
 
+        # Erzeugt jede Zeile des Spielfelds.
         for zeile in range(self.sk.zeilen):
+
+            # Zeilen ohne Zielsymbol.
             if(zeile != self.zielposition.zeile):
-                diese_zeile = zg.generiere_random_chars(self.sk.spalten, blocked_char=self.zielsymbol)
+
+                diese_zeile = zg.generiere_random_chars(
+                    self.sk.spalten,
+                    blocked_char=self.zielsymbol
+                )
+
+            # Zeile, die das Zielsymbol enthält.
             else:
-                laenge_vor   = self.zielposition.spalte
-                laenge_nach  = self.sk.spalten - self.zielposition.spalte - self.sk.zielsymbollaenge
-                diese_zeile  = zg.generiere_random_chars(laenge_vor, blocked_char=self.zielsymbol)
+                # Anzahl der Zeichen vor dem Zielsymbol.
+                laenge_vor = self.zielposition.spalte
+
+                # Anzahl der Zeichen nach dem Zielsymbol.
+                laenge_nach = (
+                    self.sk.spalten
+                    - self.zielposition.spalte
+                    - self.sk.zielsymbollaenge
+                )
+
+                # Generiert die Zeichen vor dem Zielsymbol.
+                diese_zeile = zg.generiere_random_chars(
+                    laenge_vor,
+                    blocked_char=self.zielsymbol
+                )
+
+                # Fügt das Zielsymbol ein.
                 diese_zeile += self.zielsymbol
-                diese_zeile += zg.generiere_random_chars(laenge_nach, blocked_char=self.zielsymbol)
-            diese_zeile += '\n'
+
+                # Generiert die Zeichen nach dem Zielsymbol.
+                diese_zeile += zg.generiere_random_chars(
+                    laenge_nach,
+                    blocked_char=self.zielsymbol
+                )
+
+            # Fügt die fertige Zeile dem Spielfeld hinzu.
             self.feld.append(diese_zeile)
